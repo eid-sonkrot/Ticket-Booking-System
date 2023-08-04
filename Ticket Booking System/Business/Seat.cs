8acc799ff270e777ed7e0b23087528b795a8b1aa
fdfd <eid.com>
2023-08-04 14:21:43 +0300

    "Enhance Ticket Class with Relationships and Validation"
    In this commit, the Ticket class has undergone enhancements to establish better relationships and include necessary validation attributes. The following changes have been made:
    
    1. Added 'Person' Property: The 'person' property has been introduced to associate a person with each ticket. This allows us to store detailed information about the individual holding the ticket, regardless of whether they are a passenger or a manager.
    
    2. Enhanced 'Flight' Property: The 'flight' property now links each ticket to a specific flight. This enables us to track which flight the ticket is associated with.
    
    3. Added 'Seat' Property: The 'seat' property has been included to represent the seat assigned to each ticket. This way, we can easily manage and retrieve seat information for a particular ticket.
    
    4. [Required] Attribute for 'BookingId': The 'bookingId' property now has the [Required] attribute to ensure that each ticket is associated with a valid booking. This enhances data integrity and helps maintain consistency between tickets and bookings.
    
    These changes significantly improve the Ticket class's functionality and data representation within the airport ticket booking application. By establishing appropriate relationships and validation, we ensure the accurate and efficient management of tickets and their associations with passengers, flights, and bookings.
    
    As always, your feedback on the changes and code review are highly appreciated to maintain the quality and excellence of our codebase."

---
+++ Ticket Booking System/Business/Seat.cs
@@ -0,0 +1,7 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public record Seat
+{
+    [RegularExpression("^[a-zA-Z0-9]*$", ErrorMessage = "The field must contain only alphabets and numbers.")]
+    public String SeatNumber { get; set; }
+}
--- Ticket Booking System/Business/Ticket.cs
+++ Ticket Booking System/Business/Ticket.cs
@@ -1,6 +1,15 @@
-﻿namespace TicketBookingSystem.Business
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicketBookingSystem.Business
 {
     public class Ticket
     {
+        
+        public Person person { get; set; }
+        [Required]
+        public Flight flight { get; set; }
+        public Seat seat { get; set; }
+        [Required]
+        BookingId bookingId { get; set; }
     }
 }
