64ed12d6af90d734eddbfc1df23372834d20c1c5
fdfd <eid.com>
2023-08-04 21:06:10 +0300

    Enhance Booking Class with Ticket Collection and Status Property
    In this commit, we have made significant enhancements to the Booking class by introducing a ticket collection and a status property. These additions allow for better management of reservations made by passengers for specific flights.
    
    Here are the changes in detail:
    
    1. **Ticket Collection**: We have added a private list of tickets, `List<Ticket> tickets`, to the Booking class. This collection will hold individual Ticket objects associated with the booking, representing the tickets reserved by the passenger.
    
    2. **BookingId Property**: The `bookingId` property has been added to the Booking class. This property serves as an identifier for the booking, uniquely identifying each reservation.
    
    3. **BookingDate Property**: The `bookingDate` property has been included in the Booking class. It represents the date when the booking was made, providing essential booking details.
    
    4. **BookingStatus Property**: The `bookingStatus` property has been introduced as an enumeration of type `BookingStatus`. This property allows us to represent the current status of the booking, indicating whether it is Confirmed, Canceled, On Hold, Pending Payment, or Completed.
    
    By incorporating the ticket collection and status property, the Booking class now provides a comprehensive representation of a reservation, including ticket information and the current booking status.
    
    As always, your review and feedback on the changes are highly appreciated to ensure the quality and effectiveness of this enhancement. Please feel free to share any suggestions or improvements to further enhance our application's functionality and user experience.

--- Ticket Booking System/Business/Booking.cs
+++ Ticket Booking System/Business/Booking.cs
@@ -2,5 +2,11 @@
 {
     public class Booking
     {
+        
+        private List<Ticket> tickets=new List<Ticket>();
+        public BookingId bookingId { get; set; }
+        public Date bookingDate { get; set; }
+        public BookingStatus bookingStatus { get; set; }
+        
     }
 }
