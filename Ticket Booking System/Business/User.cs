e6444fbe8e30b5712686127f55af06931825b1ba
eid <eid@DESKTOP-FPJ7SAQ>
2023-08-05 10:30:41 +0300

    [ User record]
    Added User record with properties: Person, Email, and HashPassword, along with validation attributes.
    
    In this commit, I have introduced a new User record with three properties: Person, Email, and HashPassword. Additionally, I have implemented custom validation attributes to ensure data integrity.
    
    1. Person Property: Represents the user's name or username, ensuring data consistency and clear identification.
    
    2. Email Property: Stores the user's email address, and I have added the custom [Email] attribute to validate the email format before storage, preventing incorrect email addresses from being stored.
    
    3. HashPassword Property: Holds the user's hashed password for added security. The password is never stored in plain text, and I have implemented appropriate hashing mechanisms to safeguard sensitive information.
    
    These custom validation attributes ensure that the user's data is valid and secure, providing better data quality and protection against potential vulnerabilities.
    
    Please review the changes and feel free to provide any feedback or suggestions for improvements.

---
+++ Ticket Booking System/Business/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TicketBookingSystem.Business;
+
+public record User
+{
+    public Person person { get; set; }
+    [Email(ErrorMessage = "Invalid email address.")]
+    public string email { get; set; }
+    [RegularExpression("^[a-zA-Z0-9]*$", ErrorMessage = "The hashedPassword must contain only alphabets and numbers.")]
+    public string hashedPassword { get; set; }
+
+}
