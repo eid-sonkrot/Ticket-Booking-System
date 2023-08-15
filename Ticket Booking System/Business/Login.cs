88ccdd6b7257a84af31eca1042748c103300ca89
eid <eid@DESKTOP-FPJ7SAQ>
2023-08-15 11:46:43 +0300

    Implement Login and User Authentication
    Implemented the `GetUserControall` method within the `Login` class to handle user authentication and user creation based on the provided credentials. The method performs user authentication using the `UserAuthentication` function and then uses the `userFactory` to create an instance of the appropriate user type. Errors during authentication or user creation are caught and handled, with error messages printed to the console.
    
    This commit lays the foundation for secure user login and differentiates between manager and passenger roles during the authentication process.

--- Ticket Booking System/Business/Login.cs
+++ Ticket Booking System/Business/Login.cs
@@ -1,6 +1,44 @@
 ﻿namespace TicketBookingSystem.Business
 {
     public class Login
-    {
+    {   
+        private UserFactory userFactory;
+
+        public Login()
+        {
+            userFactory = new UserFactory();
+        }
+        public IUser GetUserControall(User user,UserRole role)
+        {
+            try
+            {
+                if (UserAuthentication(user, role))
+                {
+                    return userFactory.CreateUser(user, role);
+                }else
+                {
+                    Console.WriteLine("Authentication failed.");
+                    return null;
+                }
+            }catch(Exception ex)
+            {
+                Console.WriteLine($"Error: with Creat User. {ex.Message}");
+                return null;
+            }
+        }
+        public bool UserAuthentication(User user,UserRole role)
+        {
+            try
+            {
+                var proxy = new Proxy();
+
+                return proxy.UserAuthentication(user,role);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: with Authenticat User. {ex.Message}");
+                return false;
+            }
+        }
     }
 }
